bb862f5f50976f09d5874c4e65dbf5dc64bc1c9c
haijunq <haijunq@gmail.com>
2014-04-24 22:38:31 -0700

    init

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>HTML</title>
+</head>
+<body>
+ <img src="http://imgs.tuts.dragoart.com/how-to-draw-doraemon_1_000000012089_5.gif" />
+</body>
+</html>
